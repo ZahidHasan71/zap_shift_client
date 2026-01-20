ee8c3a955f368342a0b69ce0c20e69111f681863
ZahidHasan71 <zahidhasan0000001@gmail.com>
2026-01-20 22:13:07 +0600

    nav and footer done

---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Footer from '../pages/Shared/Footer';
+import Navbar from '../pages/Shared/Navbar';
+
+const RootLayout = () => {
+    return (
+        <div className='bg-[#EAECED]'>
+            <div className='max-w-7xl mx-auto '>
+                <Navbar />
+                <Outlet />
+                <Footer />
+            </div>
+        </div>
+    );
+};
+
+export default RootLayout;
